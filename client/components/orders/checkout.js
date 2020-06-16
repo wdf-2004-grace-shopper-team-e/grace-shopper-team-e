2352adfb72cbd17988a81bdfe3098374c87b7e46
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-16 15:20:56 -0400

    created skeletons for order components

---
+++ client/components/orders/checkout.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const CheckoutPage = (props) => {
+  (
+    <div>
+      <h1>Checkout Page</h1>
+      <div>Form Info
+      <form>
+        <div>Shipping Form</div>
+        <div>Billing Info</div>
+        <button type="submit"></button>
+      </form>
+
+      </div>
+      <div>Sidebar w/ order information
+        <div>Price Info</div>
+        <div>Cart</div>
+      </div>
+    </div>
+  )
+}
+
+const mapState = state => {
+  return {
+
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(mapState, mapDispatch)(CheckoutPage)
