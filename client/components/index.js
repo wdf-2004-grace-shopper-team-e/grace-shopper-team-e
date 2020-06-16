4ac12170162d0c3445ceddd8c9093130b94ac379
Frederick Tetteh <kwesiossom@gmail.com>
2020-06-16 16:24:30 -0400

    created plants component sub-directory

--- client/components/index.js
+++ client/components/index.js
@@ -5,5 +5,5 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
-export {default as Allplants} from './AllPlants'
+export {default as Allplants} from './plants/AllPlants'
 export {Login, Signup} from './auth-form'
