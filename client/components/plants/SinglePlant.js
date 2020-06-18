e709adbfc5b9bfd32a27535043a91cd56785b49e
Frederick Tetteh <kwesiossom@gmail.com>
2020-06-18 16:41:35 -0400

    feature update: add to cart button

--- client/components/plants/SinglePlant.js
+++ client/components/plants/SinglePlant.js
@@ -7,10 +7,22 @@ import {fetchPlant} from '../../store/singlePlant'
  * Plant COMPONENT
  */
 export class Plant extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      quantityOrdered: 1
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
   componentDidMount() {
     const id = this.props.match.params.plantId
     this.props.getPlant(id)
   }
+  handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+  handleSubmit(event) {}
   getPrice = priceInPennies => {
     let dollars = priceInPennies / 100
     dollars = dollars.toLocaleString('en-US', {
@@ -37,6 +49,20 @@ export class Plant extends React.Component {
         <div style={{backgroundColor: 'lightblue'}}>
           <p>Price: {this.getPrice(plant.price)} </p>
           <p>Current Stock: {plant.stock}</p>
+          <form>
+            <input
+              name="quantityOrdered"
+              type="number"
+              value={this.state.quantityOrdered}
+              min="1"
+              max={plant.stock}
+              onChange={this.handleChange}
+              required
+            />
+            <button disabled={!plant.stock} type="submit">
+              Add to Cart
+            </button>
+          </form>
         </div>
         <div>
           <h2>Plant Overview</h2>
