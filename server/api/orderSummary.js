4c3f69c9be1cd2819c9d84cbf6a4b63ed3d9fb60
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-18 13:13:16 -0400

    started working on orderSummary API

--- server/api/orderSummary.js
+++ server/api/orderSummary.js
@@ -2,4 +2,10 @@ const router = require('express').Router()
 const {OrderSummary} = require('../db/models')
 module.exports = router
 
-//
+// get all plants associated with an order
+
+// add a plant to an order
+
+// edit specific plants associated with an order
+
+// remove specific plants associated with an order
