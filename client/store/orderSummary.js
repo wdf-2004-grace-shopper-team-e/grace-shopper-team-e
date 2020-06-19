90e53bb6dcf2f9c060d7c38b7da576f637649700
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-18 21:42:20 -0400

    built out ordersummary redux

--- client/store/orderSummary.js
+++ client/store/orderSummary.js
@@ -1,23 +1,28 @@
 import axios from 'axios'
 
 // action types
+const GET_ITEMS = 'GET_ITEMS'
 const ADD_ITEM = 'ADD_ITEM'
 const REMOVE_ITEM = 'REMOVE_ITEM'
 const UPDATE_ITEM = 'UPDATE_ITEM'
 const SUBMIT_ORDER = 'SUBMIT_ORDER'
 
 // action creators
-const addItem = item => ({
+const getItems = items => ({
+  type: GET_ITEMS,
+  items
+})
+const addItem = items => ({
   type: ADD_ITEM,
-  item
+  items
 })
-const removeItem = item => ({
+const removeItem = items => ({
   type: REMOVE_ITEM,
-  item
+  items
 })
-const updateItem = item => ({
+const updateItem = items => ({
   type: UPDATE_ITEM,
-  item
+  items
 })
 const submitOrder = order => ({
   type: SUBMIT_ORDER,
@@ -25,6 +30,58 @@ const submitOrder = order => ({
 })
 
 // thunks
+export const fetchItems = orderId => {
+  return async dispatch => {
+    try {
+      const plants = await axios.get(`/api/ordersummary/${orderId}/plants`)
+      dispatch(getItems(plants.data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const thunkAddItem = event => {
+  return async dispatch => {
+    try {
+      const {orderId, plantId} = event.target
+      const plants = await axios.post(
+        `/api/ordersummary/${orderId}/add/${plantId}`
+      )
+      dispatch(addItem(plants))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const thunkRemoveItem = event => {
+  return async dispatch => {
+    try {
+      const {orderId, plantId} = event.target
+      const plants = await axios.post(
+        `/api/ordersummary/${orderId}/remove/${plantId}`
+      )
+      dispatch(removeItem(plants))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const thunkUpdateItem = event => {
+  return async dispatch => {
+    try {
+      const {orderId, plantId, value} = event.target
+      const plants = await axios.post(
+        `/api/ordersummary/${orderId}/add/${plantId}/${value}`
+      )
+      dispatch(updateItem(plants))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
 
 // initial state
 const orderSummary = []
@@ -32,9 +89,14 @@ const orderSummary = []
 // reducer
 export default function(state = orderSummary, action) {
   switch (action.type) {
-    // ADD_ITEM
-    // REMOVE_ITEM
-    // UPDATE_ITEM
+    case GET_ITEMS:
+      return action.items
+    case ADD_ITEM:
+      return action.items
+    case REMOVE_ITEM:
+      return action.items
+    case UPDATE_ITEM:
+      return action.items
     // SUBMIT_ORDER
     default:
       return state
