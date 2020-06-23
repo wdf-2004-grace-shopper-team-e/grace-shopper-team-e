745feee2a87a15d8ea62f911fb2585ed7f17d190
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-23 19:35:44 -0400

    userupdate bug fixed

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -41,7 +41,9 @@ export class UserHome extends Component {
       const updateCart = {
         cartId: guestCart.id,
         id: this.props.id,
-        email: this.props.email
+        email: this.props.email,
+        firstName: this.props.firstName,
+        lastName: this.props.lastName
       }
       await this.props.updateUser(this.props.id, updateCart)
       await axios.put(`/api/users/${this.props.id}/set/${guestCart.id}`)
