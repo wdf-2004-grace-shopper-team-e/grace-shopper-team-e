05bb5a774b2c069e4e7886c10da64472e3340a12
Radical Ube <57785974+radical-ube@users.noreply.github.com>
2020-06-23 08:47:25 -0400

    users basic order history is viewable

--- client/components/orders/orderHistory.js
+++ client/components/orders/orderHistory.js
@@ -2,25 +2,34 @@ import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
 
 const OrderHistory = props => {
-  const {user} = props
-  const {orders} = user
-  console.log('user', user)
-
-  useEffect(() => {
-    async function fetchMe() {
-      await props.me()
-    }
-    console.log('user fetched', props.user)
-  })
-
   return (
     <div>
       <ul>
-        {/* {orders.map(order => {
-          <div key={order.id}>
-            <li>{order.id}</li>
+        {props.user.id ? (
+          <div>
+            <h1>user name {props.user.firstName}</h1>
+            {props.user.orders.map(order => {
+              if (!order.isCart) {
+                return (
+                  <div key={order.id}>
+                    <h2>Order Number {order.id}</h2>
+                    <h3>Order Total ${order.totalCost / 100}</h3>
+                    {order.OrderSummary.map(plant => {
+                      return (
+                        <div key={plant.id}>
+                          <h4>Plant Name: {plant.name}</h4>
+                          <h4>Qty: {plant.plant_order.plantQuantity}</h4>
+                        </div>
+                      )
+                    })}
+                  </div>
+                )
+              }
+            })}
           </div>
-        })} */}
+        ) : (
+          <h1>no user</h1>
+        )}
       </ul>
     </div>
   )
@@ -32,10 +41,4 @@ const mapState = state => {
   }
 }
 
-const mapDispatch = dispatch => {
-  return {
-    me: () => dispatch(me())
-  }
-}
-
-export default connect(mapState, null)(OrderHistory)
+export default connect(mapState)(OrderHistory)
